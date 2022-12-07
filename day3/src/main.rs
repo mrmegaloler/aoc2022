28e048fa588f1aff962259b1846f93395ac3bf56
Victor Lagerfors <victor@lagerfors.com>
2022-12-07 15:29:00 +0100

    day 3 complete

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,5 +1,5 @@
 use array_tool::vec::Intersect;
-use std::fs;
+use std::{fs, ops::Index};
 
 #[derive(Debug)]
 struct Rucksack {
@@ -7,11 +7,30 @@ struct Rucksack {
     lower_compartment: Vec<char>,
 }
 
+#[derive(Debug)]
+struct GroupRucksacks {
+    rucksack_one: Vec<char>,
+    rucksack_two: Vec<char>,
+    rucksack_three: Vec<char>,
+}
+
 impl Rucksack {
     fn misplace_score(&self, alphabet: &Vec<char>) -> i32 {
         let intersection = self
             .lower_compartment
             .intersect(self.upper_compartment.to_owned());
+
+        return alphabet.iter().position(|&r| r == intersection[0]).unwrap() as i32 + 1;
+    }
+}
+
+impl GroupRucksacks {
+    fn misplace_score(&self, alphabet: &Vec<char>) -> i32 {
+        let intersection = self
+            .rucksack_one
+            .intersect(self.rucksack_two.to_owned())
+            .intersect(self.rucksack_three.to_owned());
+
         print!("{:?}", intersection[0]);
 
         return alphabet.iter().position(|&r| r == intersection[0]).unwrap() as i32 + 1;
@@ -26,7 +45,7 @@ fn main() {
     let input = fs::read_to_string("input.txt").expect("Data not found");
     let rucksacks = input
         .split("\n")
-        .map(move |rucksack| {
+        .map(|rucksack| {
             let items = rucksack.len();
             let parsed_rucksack = Rucksack {
                 upper_compartment: rucksack[..(items / 2)].chars().collect(),
@@ -35,5 +54,26 @@ fn main() {
             return parsed_rucksack.misplace_score(&alphabet);
         })
         .collect::<Vec<i32>>();
-    println!("{:?}", rucksacks.iter().sum::<i32>())
+
+    let all_rucksacks: Vec<GroupRucksacks> = input
+        .split("\n")
+        .collect::<Vec<&str>>()
+        .chunks(3)
+        .map(|chunk| {
+            let group = chunk.to_vec();
+            return GroupRucksacks {
+                rucksack_one: group[0].chars().collect(),
+                rucksack_two: group[1].chars().collect(),
+                rucksack_three: group[2].chars().collect(),
+            };
+        })
+        .collect();
+
+    let group_rucksack_score = all_rucksacks
+        .iter()
+        .map(|group_rucksack| {
+            return group_rucksack.misplace_score(&alphabet);
+        })
+        .collect::<Vec<i32>>();
+    println!("Score {}", group_rucksack_score.iter().sum::<i32>());
 }
