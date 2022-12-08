14d31ada199173c93cd5e8d0e26c5d10ee60877c
Victor Lagerfors <victor@lagerfors.com>
2022-12-08 15:19:02 +0100

    day4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,19 +1,43 @@
 use std::fs;
 
-struct WorkPairs {
-    elf_one: Vec<i32>,
-    elf_two: Vec<i32>,
+#[derive(Debug)]
+struct WorkPair {
+    elf_one: (i32, i32),
+    elf_two: (i32, i32),
 }
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Data not found");
-    let workorders: Vec<Vec<Vec<&str>>> = input
+    let workorders: Vec<WorkPair> = input
         .split("\n")
         .map(|row| row.split(",").collect::<Vec<&str>>())
         .map(|elf| {
-            let elf_one = elf[0].split("-").collect::<Vec<&str>>();
-            let elf_two = elf[1].split("-").collect::<Vec<&str>>();
-            return vec![elf_one, elf_two];
+            let elf_one = elf[0]
+                .split("-")
+                .map(|val| val.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>();
+            let elf_two = elf[1]
+                .split("-")
+                .map(|val| val.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>();
+            return WorkPair {
+                elf_one: (elf_one[0], elf_one[1]),
+                elf_two: (elf_two[0], elf_two[1]),
+            };
         })
         .collect();
-    println!("{:?}", workorders)
+
+    let temp = workorders
+        .iter()
+        .map(|pair| {
+            if (pair.elf_one.0 >= pair.elf_two.0 && pair.elf_one.1 <= pair.elf_two.1)
+                || (pair.elf_two.0 >= pair.elf_one.0 && pair.elf_two.1 <= pair.elf_one.1)
+            {
+                return 1;
+            } else {
+                return 0;
+            }
+        })
+        .collect::<Vec<i32>>();
+
+    println!("{:?}", temp.iter().sum::<i32>())
 }
